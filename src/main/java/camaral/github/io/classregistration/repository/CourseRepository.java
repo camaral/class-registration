c1be3a358f4110c590a8a40ab66f178aa529d5b3
camaral <camaral@users.noreply.github.com>
2022-07-14 13:00:23 -0300

    feat: find empty courses and students with no courses

--- src/main/java/camaral/github/io/classregistration/repository/CourseRepository.java
+++ src/main/java/camaral/github/io/classregistration/repository/CourseRepository.java
@@ -1,9 +1,12 @@
 package camaral.github.io.classregistration.repository;
 
 import camaral.github.io.classregistration.entity.CourseEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource(path = "courses", itemResourceRel = "course", collectionResourceRel = "courses")
-public interface CourseRepository extends CrudRepository<CourseEntity, Integer> {
+public interface CourseRepository extends PagingAndSortingRepository<CourseEntity, Integer> {
+    List<CourseEntity> findAllByStudentsNull();
 }
--- src/main/java/camaral/github/io/classregistration/repository/StudentRepository.java
+++ src/main/java/camaral/github/io/classregistration/repository/StudentRepository.java
@@ -1,9 +1,12 @@
 package camaral.github.io.classregistration.repository;
 
 import camaral.github.io.classregistration.entity.StudentEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource(path = "students", itemResourceRel = "student", collectionResourceRel = "students")
-public interface StudentRepository extends CrudRepository<StudentEntity, Integer> {
+public interface StudentRepository extends PagingAndSortingRepository<StudentEntity, Integer> {
+    List<StudentEntity> findAllByCoursesNull();
 }
